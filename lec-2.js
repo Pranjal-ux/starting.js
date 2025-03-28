01dd779fdef1867bbbcefbbe3aa750cf24d08a70
Pranjal-ux <pranjal2shukla@gmail.com>
2025-03-28 11:08:20 +0530

    lecture1.js

---
+++ lec-2.js
@@ -0,0 +1,14 @@
+//Airthmstic operstor//
+
+// let a = 3;
+// let b =5;
+// c= a+b;
+// console.log(c)
+
+// let e= 3;
+// let j =5;
+// k = e-j;
+// console.log(k)
+let a = 5;
+console.log("++a =",++a);
+
---
+++ start1.js
@@ -0,0 +1,8 @@
+// console.log("hello");
+
+// age=24
+// console.log(age);
+let age=56;
+age=90;
+age=75;
+console.log(age);
